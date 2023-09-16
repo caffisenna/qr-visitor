26ec820774a09fa9d927b0d13d1f0947a92818eb
caffi <caffi.senna@gmail.com>
2023-09-16 13:27:21 +0900

    集計画面

--- resources/views/visitors/sum.blade.php
+++ resources/views/visitors/sum.blade.php
@@ -1,39 +1,31 @@
-{{ dd($counts) }}
-<div class="card-body p-0">
-    <div class="table-responsive">
-        <table class="table" id="visitors-table">
-            <thead>
-                <tr>
-                    <th>ブース</th>
-                    <th>チェック</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($visitors as $visitors)
-                    <tr>
-                        <td>{{ $visitors->booth_number }}</td>
-                        <td>{{ $visitors->created_at }}</td>
-                        <td style="width: 120px">
-                            {!! Form::open(['route' => ['visitors.destroy', $visitors->id], 'method' => 'delete']) !!}
-                            <div class='btn-group'>
-                                {!! Form::button('<i class="far fa-trash-alt"></i>', [
-                                    'type' => 'submit',
-                                    'class' => 'btn btn-danger btn-xs',
-                                    'onclick' => "return confirm('Are you sure?')",
-                                ]) !!}
-                            </div>
-                            {!! Form::close() !!}
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+{{-- {{ dd($counts) }} --}}
+@extends('layouts.app')
 
-    <div class="card-footer clearfix">
-        <div class="float-right">
-            {{-- @include('adminlte-templates::common.paginate', ['records' => $visitors]) --}}
+@section('content')
+    <div class="card-body p-0">
+        <div class="table-responsive">
+            <table class="table" id="visitors-table">
+                <thead>
+                    <tr>
+                        <th>ブース</th>
+                        <th>通過人数</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($counts as $count)
+                        <tr>
+                            <td>{{ $count->booth_number }}</td>
+                            <td>{{ $count->count_booth_number }}人</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div class="card-footer clearfix">
+            <div class="float-right">
+                {{-- @include('adminlte-templates::common.paginate', ['records' => $visitors]) --}}
+            </div>
         </div>
     </div>
-</div>
+@endsection
