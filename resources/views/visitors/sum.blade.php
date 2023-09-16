eaf2322fbcc257a254162ac66c2d56a3934fdce0
caffi <caffi.senna@gmail.com>
2023-09-16 13:57:20 +0900

    Update sum.blade.php

--- resources/views/visitors/sum.blade.php
+++ resources/views/visitors/sum.blade.php
@@ -7,7 +7,7 @@
             <table class="table" id="visitors-table">
                 <thead>
                     <tr>
-                        <th>ブース</th>
+                        <th>ブース1</th>
                         <th>通過人数</th>
                     </tr>
                 </thead>
