3603a90cb7afe58fbb5d1fe4f3b70883897add67
caffi <caffi.senna@gmail.com>
2023-09-16 13:27:11 +0900

    アクティブリンクの修正

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,13 +1,13 @@
 
 <li class="nav-item">
-    <a href="{{ route('visitors.index') }}" class="nav-link {{ Request::is('visitors*') ? 'active' : '' }}">
+    <a href="{{ route('visitors.index') }}" class="nav-link {{ Request::is('visitors') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>入場者</p>
     </a>
 </li>
 
 <li class="nav-item">
-    <a href="{{ url('visitors_sum') }}" class="nav-link {{ Request::is('visitors_sum') ? 'active' : '' }}">
+    <a href="{{ url('visitors_sum') }}" class="nav-link {{ Request::is('visitors_sum*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>集計</p>
     </a>
