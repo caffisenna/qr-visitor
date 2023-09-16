e07332ba52a75c1defbfc22f6b9f1b84b159bc12
caffi <caffi.senna@gmail.com>
2023-09-16 14:24:44 +0900

    Revert "Revert "Update sum.blade.php""
    This reverts commit 6cba7029d1b2b237e5b05a2e66b8576849649044.

--- resources/views/visitors/sum.blade.php
+++ resources/views/visitors/sum.blade.php
@@ -7,7 +7,7 @@
             <table class="table" id="visitors-table">
                 <thead>
                     <tr>
-                        <th>ブース</th>
+                        <th>ブース123</th>
                         <th>通過人数</th>
                     </tr>
                 </thead>
