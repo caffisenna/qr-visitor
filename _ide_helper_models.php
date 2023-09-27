34a26359ae0bac3d6eff40f04792efdf1530be67
caffi <caffi.senna@gmail.com>
2023-09-27 22:06:48 +0900

    ide_helper

---
+++ _ide_helper_models.php
@@ -0,0 +1,65 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\visitors
+ *
+ * @property int $id
+ * @property string $booth_number
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|visitors newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|visitors newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|visitors query()
+ * @method static \Illuminate\Database\Eloquent\Builder|visitors whereBoothNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|visitors whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|visitors whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|visitors whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|visitors whereUpdatedAt($value)
+ */
+	class visitors extends \Eloquent {}
+}
+
