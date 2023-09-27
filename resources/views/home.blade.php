fdc2e374ba6e348352ed028aaeb030cedc155b11
caffi <caffi.senna@gmail.com>
2023-09-27 17:19:01 +0900

    Update home.blade.php
    - inputboxの大型化
    - 受け入れ文字列の制御etc

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,49 @@
 @section('content')
     <div class="container">
         @include('flash::message')
+        <h2>QRコードスキャン</h2>
+        <p class="uk-text-danger">↓の入力ボックスにカーソルが点滅していることを確認してQRコードをスキャンしてください。</p>
         <div class="row">
             <form method="POST" action="{{ route('visitors.store') }}" id="myForm">
                 @csrf
-                <input type="text" name="booth_number" id="booth_number" oninput="checkInputLength(this)" required>
+                <input type="text" name="booth_number" id="booth_number" oninput="limitInput(this)" maxlength="1" required
+                    class="uk-input uk-form-large">
             </form>
 
             <script>
                 document.addEventListener("DOMContentLoaded", function() {
-                    document.getElementById("booth_number").focus();
+                    var booth_numberInput = document.getElementById("booth_number");
+
+                    booth_numberInput.addEventListener("blur", function() {
+                        // フォーカスが外れた場合、再びフォーカスを設定します。
+                        booth_numberInput.focus();
+                    });
+
+                    booth_numberInput.focus(); // ページ読み込み時にフォーカスを設定します。
                 });
 
-                function checkInputLength(input) {
-                    if (input.value.length === 6) {
+                function limitInput(input) {
+                    var allowedCharacters = ['H', 'K', 'S', 'E', 'F'];
+                    var inputValue = input.value.toUpperCase();
+
+                    // 入力が許可された文字かどうかを確認し、許可されていない場合は削除します。
+                    if (allowedCharacters.indexOf(inputValue) === -1) {
+                        input.value = '';
+                    }
+
+                    if (input.value.length === 1) {
                         document.getElementById('myForm').submit();
                     }
                 }
+            </script>
+
+            <script>
+                setTimeout(function() {
+                    var flashMessage = document.querySelector('.alert'); // Flashメッセージの要素を取得
+                    if (flashMessage) {
+                        flashMessage.remove(); // 要素を削除
+                    }
+                }, 5000); // 5秒後に削除する
             </script>
         </div>
     </div>
