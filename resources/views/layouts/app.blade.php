0490973ead56b2bd438e741d0c63894ea965fd09
caffi <caffi.senna@gmail.com>
2023-09-30 00:58:32 +0900

    Update app.blade.php
    日本語化

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,19 +66,19 @@
                     <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                         <!-- User image -->
                         <li class="user-header bg-primary">
-                            <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
-                                class="img-circle elevation-2" alt="User Image">
+                            {{-- <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
+                                class="img-circle elevation-2" alt="User Image"> --}}
                             <p>
                                 {{ Auth::user()->name }}
-                                <small>Member since {{ Auth::user()->created_at->format('M. Y') }}</small>
+                                {{-- <small>Member since {{ Auth::user()->created_at->format('M. Y') }}</small> --}}
                             </p>
                         </li>
                         <!-- Menu Footer-->
                         <li class="user-footer">
-                            <a href="#" class="btn btn-default btn-flat">Profile</a>
+                            {{-- <a href="#" class="btn btn-default btn-flat">Profile</a> --}}
                             <a href="#" class="btn btn-default btn-flat float-right"
                                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                Sign out
+                                ログアウト
                             </a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
